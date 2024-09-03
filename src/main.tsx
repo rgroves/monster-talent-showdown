31666df9028c09a876988d31d994620aaea0ba64
Robert Groves <rwgdev@gmail.com>
2024-09-02 19:10:33 -0500

    Add Convex provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import { ClerkProvider } from "@clerk/clerk-react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ClerkProvider } from "@clerk/clerk-react";
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -11,10 +12,14 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
 }
 
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+      <ConvexProvider client={convex}>
+        <App />
+      </ConvexProvider>
     </ClerkProvider>
   </StrictMode>
 );
