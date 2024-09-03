516ad3142b3e10b6f0697bca08f68f5d6880f4f6
Robert Groves <rwgdev@gmail.com>
2024-09-03 00:25:32 -0500

    Add create game mutation

--- convex/games.ts
+++ convex/games.ts
@@ -1,4 +1,31 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+
+enum GameState {
+  JOINING = "JOINING",
+  INPROGRESS = "INPROGRESS",
+  COMPLETED = "COMPLETED",
+  ABANDONED = "ABANDONED",
+}
+
+export const create = mutation({
+  args: {},
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Failed to create game: Unauthenticated");
+    }
+
+    const joinCode = crypto.randomUUID();
+
+    await ctx.db.insert("games", {
+      ownerUserId: identity.subject,
+      joinCode,
+      status: GameState.JOINING,
+    });
+
+    return joinCode;
+  },
+});
 
 export const get = query({
   args: {},
