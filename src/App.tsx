567f23939ca7a51c8d493c34c86142d131836232
Robert Groves <rwgdev@gmail.com>
2024-09-02 19:11:00 -0500

    Add game data retrieval for minimal deployt test

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,30 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
 import "./App.css";
 
 export default function App() {
+  const games = useQuery(api.games.get);
+
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <main>
+        {games?.map(({ _id, gameId, state }) => (
+          <div key={_id}>
+            {gameId} - [{state}]
+          </div>
+        ))}
+      </main>
+    </>
   );
 }
