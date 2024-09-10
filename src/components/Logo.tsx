b14e7d5c4048d385eb5a2e0e379592fed68821c4
Robert Groves <rwgdev@gmail.com>
2024-09-10 01:14:00 -0500

    Add logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+export default function Logo() {
+  const blackAlphaLogoPath = "./logo-black-alpha-400.png";
+
+  return (
+    <img
+      src={blackAlphaLogoPath}
+      height="125"
+      width="125"
+      className="max-w-[125]"
+    />
+  );
+}
