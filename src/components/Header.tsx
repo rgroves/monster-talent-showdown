92f92ee21d244e14f9b2ea4841a02934c57cfb49
Robert Groves <rwgdev@gmail.com>
2024-09-10 01:14:46 -0500

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
 import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
+import { Button } from "./ui/button";
+import Logo from "./Logo";
 
 export default function Header({}) {
   return (
-    <header>
+    <header className="flex items-center justify-between md:sticky md:top-0">
+      <Logo />
+      <p className="sr-only">Monster Talent Showdown</p>
       <AuthLoading>Loading...</AuthLoading>
       <Unauthenticated>
-        <SignInButton mode="modal" />
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
       </Unauthenticated>
       <Authenticated>
         <UserButton />
