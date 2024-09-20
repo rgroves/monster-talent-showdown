9b09b2d23f43c2e39edf3ecac5605723ebb54294
Robert Groves <rwgdev@gmail.com>
2024-09-20 11:34:52 -0500

    Update deck builder for new monster attributes

--- sample-data/deck-builder.js
+++ sample-data/deck-builder.js
@@ -1,44 +1,63 @@
 const version = process.argv[2];
 
-console.log({ args: process.argv, version });
-
 const dayMonsters = ["Fairy", "Imp", "Sasquatch", "Leprechaun"];
 const nightMonsters = ["Vampire", "Werewolf", "Banshee", "Kitsune"];
 const anytimeMonsters = ["Mummy", "Golem", "Zombie", "Ghost"];
 
 function createMonsterCard(monsterType, timeAdvantage) {
   return {
+    id: "",
     monsterType,
     timeAdvantage,
+    primaryAbility: undefined,
+    secondaryAbility: undefined,
   };
 }
 
 function shuffle(deck) {
   for (let i = deck.length - 1; i >= 1; i--) {
     const j = Math.floor(Math.random() * i);
-    // console.log(
-    //   `--> Swapping: ${i} <-> ${j} | ${JSON.stringify(deck[i])} <-> ${JSON.stringify(deck[j])}`
-    // );
     const temp = deck[j];
     deck[j] = deck[i];
     deck[i] = temp;
-    // console.log(
-    //   `--> Swapped: ${i} *-* ${j} | ${JSON.stringify(deck[i])} *-* ${JSON.stringify(deck[j])}`
-    // );
   }
 }
 
-const deck = [
+const allMonsters = [
   ...dayMonsters.map((monster) => createMonsterCard(monster, "DAY")),
   ...nightMonsters.map((monster) => createMonsterCard(monster, "NIGHT")),
   ...anytimeMonsters.map((monster) => createMonsterCard(monster, "ANY")),
 ];
 
+const variants = ["Acting", "Comedy", "Dancing", "Singing"];
+
+const deck = [];
+let cardCount = 0;
+
+for (let i = 0; i < allMonsters.length; i++) {
+  for (let j = 0; j < variants.length; j++) {
+    for (let k = 0; k < variants.length; k++) {
+      if (k === j) {
+        continue;
+      }
+
+      allMonsters[i].primaryAbility = variants[j];
+      allMonsters[i].secondaryAbility = variants[k];
+      deck.push(Object.assign({}, allMonsters[i]));
+      console.log(
+        `${++cardCount}. ${allMonsters[i].monsterType} - ${variants[j]} | ${variants[k]}`,
+      );
+    }
+  }
+}
+
 // console.log(">>>");
 // console.log(deck.map((card, idx) => `${idx}. ${JSON.stringify(card)}`));
 
+console.log(">>> Non-Shuffled");
 console.log(JSON.stringify(deck));
 shuffle(deck);
+console.log(">>> Shuffled");
 console.log(JSON.stringify(deck));
 
 // console.log(">>>");
